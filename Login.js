296ed610c312fcf5ec51a2a0e51443084b9daf09
den1s0v <hello-lumen@ya.ru>
2019-04-13 01:38:42 +0400

    U fix .js file added on branch benchmark

--- Login.js
+++ Login.js
@@ -6,7 +6,6 @@ import Form from 'react-bootstrap/lib/Form'
 import Button from 'react-bootstrap/lib/Button'
 
 import { GoogleButton } from "./social-buttons/GoogleButton";
-import { VkButton } from "./social-buttons/VkButton";
 
 export class Login extends Component {
     constructor(props) {
@@ -39,7 +38,6 @@ export class Login extends Component {
 					alert("You're log in! :3")
 					window.localStorage.setItem("userToken", JSON.stringify(userInfo.token));
 					console.log(window.localStorage.getItem("userToken"));
-//					console.log(JSON.stringify(userInfo, null, 2));
 				}
 			});
 		}
